67d36bdcfb865bfd4150fd5dbbee646395b09fb7
siddharth gupta <sidakash014@gmail.com>
2023-07-04 17:56:02 +0530

    made changes in model

--- src/main/java/com/mountblue/instagram/model/User.java
+++ src/main/java/com/mountblue/instagram/model/User.java
@@ -3,29 +3,15 @@ package com.mountblue.instagram.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.Document;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "users")
 public class User {
     private String username;
     private String email;
     private String password;
-
-    public Document toDocument() {
-        Document document = new Document();
-        document.append("username", username);
-        document.append("email", email);
-        document.append("password", password);
-        return document;
-    }
-
-    public static User fromDocument(Document document) {
-        String username = document.getString("username");
-        String email = document.getString("email");
-        String password = document.getString("password");
-        return new User(username, email, password);
-    }
 }
 

09d54433cbd9cf682aae5e5f7a86d857481ab199
siddharth gupta <sidakash014@gmail.com>
2023-07-05 16:12:30 +0530

    made changes in User class, repo, rest controller and service.

--- src/main/java/com/mountblue/instagram/model/User.java
+++ src/main/java/com/mountblue/instagram/model/User.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
@@ -14,13 +14,15 @@ import java.util.ArrayList;
 @AllArgsConstructor
 @Document(collection = "users")
 public class User {
+
     @Id
-    @Indexed(unique = true)
+    private ObjectId id;
     private String userName;
     private String email;
     private String fullName;
     private String password;
     private ArrayList<String> followers;
     private ArrayList<String> following;
+    private String bio;
 }
 
--- src/main/java/com/mountblue/instagram/restcontroller/UserRestController.java
+++ src/main/java/com/mountblue/instagram/restcontroller/UserRestController.java
@@ -2,6 +2,7 @@ package com.mountblue.instagram.restcontroller;
 
 import com.mountblue.instagram.model.User;
 import com.mountblue.instagram.repository.UserRepository;
+import com.mountblue.instagram.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +13,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
-
     @Autowired
     private final UserRepository userRepository;
+    @Autowired
+    private final UserService userService;
 
-    public UserRestController(UserRepository userRepository) {
+    public UserRestController(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
     }
+
     @GetMapping("/getAllUser")
     public List<User> getAllUser(){
         return userRepository.findAll();
@@ -28,5 +32,26 @@ public class UserRestController {
     public ResponseEntity<String> saveUser(@RequestBody User user) {
         userRepository.save(user);
         return new ResponseEntity<>("User Saved Successfully", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/deleteUser")
+    public ResponseEntity<String> deleteUser(@RequestBody User existUser) {
+        User user = userRepository.findByUserName(existUser.getUserName());
+        if (user != null) {
+            userRepository.deleteByUserName(user.getUserName());
+            return new ResponseEntity<>("User Deleted Successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("User is not found", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/updateUser")
+    public ResponseEntity<String> updateUser(@RequestBody User user) {
+        if (userRepository.findByUserName(user.getUserName()) != null) {
+            userService.updateUser(user);
+            return new ResponseEntity<>("Post Has been updated", HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>("Post with id is not present please create the new post", HttpStatus.NOT_FOUND);
+        }
     }
 }
